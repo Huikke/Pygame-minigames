5cb598b3d60213486d546d08b083810bacc13066
Lassi Chen <lachen@utu.fi>
2023-12-25 12:51:53 +0200

    added apple

--- Snake/game.py
+++ Snake/game.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
+from random import randint
 
-# Required
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
@@ -11,12 +11,12 @@ font = pygame.font.SysFont(None, 50)
 # Window title
 pygame.display.set_caption("Snake")
 
-# Fill with your own code
+# Load art
 wall = pygame.image.load("art/wall.png").convert()
 ground = pygame.image.load("art/ground.png").convert()
 snake_head = pygame.image.load("art/snake_head.png").convert()
 snake_body = pygame.image.load("art/snake_body.png").convert()
-text_surface = font.render("My Game", False, "Purple")
+apple = pygame.image.load("art/apple.png").convert_alpha()
 
 # Make the board
 # 0 = ground, "w" = wall, 1..n = body, n+1 = head
@@ -27,13 +27,27 @@ for i in range(0, 16):
             board[i][j] = "w"
 board[2][3] = 2
 board[2][2] = 1
+# debug
 for i in board:
     print(i)
 
+# Gameplay related variables
 snake_len = 2
 direction = (0, 1)
 fps_timer = 0
 
+# Ready()
+# Set Apple location
+while True:
+    apple_x = randint(1,15)
+    apple_y = randint(1,15)
+    if apple_x == 2 and apple_y == 2 or apple_x == 2 and apple_y == 3:
+        continue
+    else:
+        break
+board[apple_x][apple_y] = "a"
+
+# Process()
 while True:
     # Takes care of closing the program
     for event in pygame.event.get():
@@ -41,24 +55,30 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # Blit walls
     for i in range(0,800,50):
         screen.blit(wall,(i,0))
         screen.blit(wall,(0,i))
         screen.blit(wall,(750,i))
         screen.blit(wall,(i,750))
 
+    # Blit grounds
     for i in range(50, 750, 50):
         for j in range(50, 750, 50):
             screen.blit(ground,(i,j))
 
+    # Blit snake
     for i in range(1,15):
         for j in range(1,15):
             if board[i][j] != 0:
                 if board[i][j] == snake_len:
                     screen.blit(snake_head,(j*50,i*50))
+                elif board[i][j] == "a":
+                    screen.blit(apple,(apple_y*50,apple_x*50))
                 else:
                     screen.blit(snake_body,(j*50,i*50))
 
+    # Observes keys
     key_down = pygame.key.get_pressed()
     if key_down[pygame.K_LEFT] or key_down[pygame.K_a]:
         direction = (0, -1)
@@ -74,7 +94,7 @@ while True:
         fps_timer = 0
         for i in range(1,15):
             for j in range(1,15):
-                if board[i][j] != 0:
+                if board[i][j] != 0 and board[i][j] != "a":
                     board[i][j] -= 1
         for i in range(1,15):
             for j in range(1,15):
@@ -86,6 +106,7 @@ while True:
             print(i)
     else:
         fps_timer += 1
-    # Takes care of updating the game and FPS
+
+
     pygame.display.update()
     clock.tick(60)
