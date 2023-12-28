116dba5054abe03f0e4f47ed4e15854a7e10fd2a
Lassi Chen <lachen@utu.fi>
2023-12-29 01:51:30 +0200

    game can be restart after game end

--- Snake/game.py
+++ Snake/game.py
@@ -30,27 +30,9 @@ pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
 font_big = pygame.font.Font("font/PressStart2P-Regular.ttf", 50)
-font_med = pygame.font.Font("font/PressStart2P-Regular.ttf", 20)
+font_med = pygame.font.Font("font/PressStart2P-Regular.ttf", 26)
+font_lil = pygame.font.Font("font/PressStart2P-Regular.ttf", 18)
 pygame.display.set_caption("Snake")
-
-# Make the board
-# 0 = ground, "w" = wall, 1..n = body, n+1 = head
-board = [[0 for _ in range(0, 16)] for _ in range(0, 16)]
-for i in range(0, 16):
-    for j in range(0, 16):
-        if i == 0 or j == 0 or i == 15 or j == 15:
-            board[i][j] = "w"
-board[2][3] = 2
-board[2][2] = 1
-# debug
-for i in board:
-    print(i)
-
-# Gameplay related variables
-snake_len = 2
-direction = (0, 1)
-fps_timer = 0
-game_over = False
 
 # Load art
 wall = pygame.image.load("art/wall.png").convert()
@@ -63,7 +45,8 @@ text_game_over = font_big.render("Game Over", False, "Black")
 text_game_over_rect = text_game_over.get_rect(center = (400, 400))
 
 # Ready()
-apple_x, apple_y = spawn_apple()
+game_over = False
+game_start = True
 
 # Process()
 while True:
@@ -72,13 +55,35 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    key_down = pygame.key.get_pressed()
 
     if game_over == False:
-        # Gameplay related variables
-        grow = False
+        # Run when a new game is being started
+        if game_start:
+            # Make the board
+            # 0 = ground, "w" = wall, 1..n = body, n+1 = head
+            board = [[0 for _ in range(0, 16)] for _ in range(0, 16)]
+            for i in range(0, 16):
+                for j in range(0, 16):
+                    if i == 0 or j == 0 or i == 15 or j == 15:
+                        board[i][j] = "w"
+            board[2][3] = 2
+            board[2][2] = 1
+
+            # Gameplay related variables
+            snake_len = 2
+            direction = (0, 1)
+            fps_timer = 0
+            grow = False
+
+            # Spawn the first apple
+            apple_x, apple_y = spawn_apple()
+
+            game_start = False
+
 
         # Observes keys
-        key_down = pygame.key.get_pressed()
         if key_down[pygame.K_LEFT] or key_down[pygame.K_a]:
             direction = (0, -1)
         if key_down[pygame.K_RIGHT] or key_down[pygame.K_d]:
@@ -119,6 +124,7 @@ while True:
                             board[i][j] -= 1
             else:
                 apple_x, apple_y = spawn_apple()
+                grow = False
 
             # places head to next tile
             board[next_tile[0]][next_tile[1]] = snake_len
@@ -164,7 +170,13 @@ while True:
         text_score = font_med.render(f"Score: {snake_len - 2}", False, "Black")
         text_score_rect = text_score.get_rect(midleft = (175, 475))
         screen.blit(text_score, text_score_rect)
+        text_restart = font_lil.render(f"Press Space to restart", False, "Black")
+        text_restart_rect = text_restart.get_rect(midleft = (175, 525))
+        screen.blit(text_restart, text_restart_rect)
 
+        if key_down[pygame.K_SPACE]:
+            game_over = False
+            game_start = True
 
 
     pygame.display.update()
